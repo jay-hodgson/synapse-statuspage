a6ef2382a38c7d85b6528983896b3c297f3238e7
jay-hodgson <jay.hodgson@sagebase.org>
2018-08-07 11:03:36 -0700

    functional

--- README.md
+++ README.md
@@ -11,10 +11,15 @@ node-lambda package -n synapse-statuspage -e production
 ```
 ## <bucket_name> is the name of the bucket where the package resides
 ## <lambda_role_arn> is the arn of the role to be assumed by the lambda function
-aws lambda create-function --region <region> --function-name synapse-statuspage --code S3Bucket=<bucket_name>,S3Key=synapse-statuspage-production.zip --role <lambda_role_arn> --handler index.handler --runtime nodejs8.10 --timeout 10 --memory-size 1024
+aws lambda create-function --region <region> --function-name synapse-statuspage --code S3Bucket=<bucket_name>,S3Key=synapse-statuspage-production.zip --role <lambda_role_arn> --handler index.handler --runtime nodejs8.10 --timeout 3 --memory-size 128
 ```
 ## How to test locally
-Use the following command to run your Amazon Lambda index.js file locally. It passes event.json data to the Amazon Lambda event object:
+Use the following command to run your Amazon Lambda index.js file locally. It passes event.json data to the Amazon Lambda event object, and uses env variables defined in deploy.env:
 ```
-node-lambda run
+node-lambda run -f deploy.env
 ```
+
+## Statuspage.io API key
+The key should be available in the deployment configuration (env variables), with key `STATUS_PAGE_IO_API_KEY`.
+
+In local testing, I set this key/value in deploy.env (which is in .gitignore, so not checked into version control).
--- index.js
+++ index.js
@@ -1,11 +1,112 @@
-// For development/testing purposes
 exports.handler = function (event, context, callback) {
     console.log('Running index.handler');
     console.log('==================================');
     console.log('event', event);
     console.log('==================================');
-    console.log('Stopping index.handler');
-    callback(null, event);
-    // or
-    // callback( 'some error type' );
+    testRepo(callback);
 };
+
+function testRepo(callback) {
+    var https = require('https');
+    var url = 'https://repo-prod.prod.sagebase.org/repo/v1/admin/synapse/status';
+    https.get(url, function (res) {
+        console.log('Got response for repo stack status: ' + res.statusCode);
+        // assemble the response body and check for down
+        var body = '';
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+        res.on('end', function () {
+            console.log('Response body: ', body);
+            if (res.statusCode == 200) {
+                var jsonBody = JSON.parse(body);
+                if (jsonBody.status !== 'READ_WRITE') {
+                    updateRepoStatus('under_maintenance', callback, jsonBody.currentMessage);
+                } else {
+                    updateRepoStatus('operational', callback, jsonBody.currentMessage);
+                }
+            } else {
+                updateRepoStatus('major_outage', callback, res.statusCode + ' - ' + res.statusMessage);
+            }
+        });
+    }).on('error', function (e) {
+        updateRepoStatus('major_outage', callback, e.message);
+    });
+}
+
+function updateRepoStatus(componentStatus, callback, error) {
+    if (error) {
+        console.log("Got repo error: " + error);
+    }
+    var componentId = 'sb280jd7bbs6';
+    updateStatusIoComponent(componentId, componentStatus);
+    testWebsite(callback);
+}
+
+function updateWebsiteStatus(componentStatus, callback, error) {
+    if (error) {
+        console.log("Got website error: " + error);
+    }
+    var componentId = 'dcgr2fz40pqc';
+    updateStatusIoComponent(componentId, componentStatus, callback);
+}
+
+function testWebsite(callback) {
+    var https = require('https');
+    var url = 'https://www.synapse.org';
+    https.get(url, function (res) {
+        console.log('Got response for website: ' + res.statusCode);
+        if (res.statusCode !== 200) {
+            updateWebsiteStatus('major_outage', callback, res.statusCode + ' - ' + res.statusMessage);
+        } else {
+            updateWebsiteStatus('operational', callback);
+        }
+    }).on('error', function (e) {
+        updateWebsiteStatus('major_outage', callback, e.message);
+    });
+}
+
+function updateStatusIoComponent(componentId, componentStatus, callback) {
+    var https = require('https');
+    // get STATUS_PAGE_IO_API_KEY from env
+    var apikey = process.env["STATUS_PAGE_IO_API_KEY"];
+    var url = 'api.statuspage.io';
+    var pageId = 'kh896k90gyvg';
+    var postData = 'component[status]=' + componentStatus;
+
+    var options = {
+        hostname: url,
+        port: 443,
+        path: '/v1/pages/'+pageId+'/components/'+componentId+'.json',
+        method: 'PATCH',
+        headers: {
+            'Authorization': 'OAuth '+apikey,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': postData.length
+        }
+    };
+
+    var req = https.request(options, (res) => {
+        console.log('statusCode:', res.statusCode);
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+            if (callback) {
+                console.log('Stopping index.handler');
+                callback(null, 'done');
+            }
+        });
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+        if (callback) {
+            callback(e);
+        }
+    });
+
+    req.write(postData);
+    req.end();
+}
+
